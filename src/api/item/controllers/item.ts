82c738fa74b22d4a63d243fc55447438c38e3c27
SrMoffat <ngigemoffat@gmail.com>
2023-06-21 12:15:02 +0300

    feat(item): add controller scaffold to create item

--- src/api/item/controllers/item.ts
+++ src/api/item/controllers/item.ts
@@ -1,7 +1,29 @@
 /**
  * item controller
  */
-
+import { get } from 'lodash';
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::item.item');
+import {
+    ITEM_API_PATH
+} from '../../../../constants';
+
+export default factories.createCoreController(`${ITEM_API_PATH}`, ({ strapi }) => ({
+    async createNewSupplyChainItem(ctx) {
+        try {
+            const auth = get(ctx.state.auth, 'credentials');
+            const requestBody = get(ctx.request, 'body') as Object;
+
+            console.log({
+                auth,
+                requestBody
+            })
+            ctx.body = {
+                success: true,
+                message: "Item controller finished successfully"
+            };
+        } catch (err) {
+            ctx.body = err;
+        };
+    },
+}));
