1b2581b5964a989f66cc521895b82beb71d929fc
4fr0c0d3 <31407254+SrMoffat@users.noreply.github.com>
2023-06-22 16:22:57 +0300

    Validate Create Item (#5)
    feat(item): add JSOn validation to controller
    chore(item): remove redundant table
    feat(item): add item entry into DB

--- src/api/handling/routes/handling.ts
+++
@@ -1,7 +0,0 @@
-/**
- * handling router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::handling.handling');
--- src/api/item/controllers/item.ts
+++ src/api/item/controllers/item.ts
@@ -1,32 +1,122 @@
 /**
  * item controller
  */
-import { get } from 'lodash';
-import { factories } from '@strapi/strapi'
+import * as crypto from 'crypto';
 
+import Ajv from 'ajv';
+import { uuid } from 'uuidv4';
+import { get } from 'lodash';
+import { factories, Strapi } from '@strapi/strapi';
+
+import itemSchema from '../schemas/item.json';
 import {
     ITEM_API_PATH
 } from '../../../../constants';
 
-export default factories.createCoreController(`${ITEM_API_PATH}`, ({ strapi }) => ({
+type Dimensions = {
+    length: string
+    width: string
+    height: string
+    unit: string
+}
+
+type Handling = {
+    type: string
+    instructions: string
+}
+
+type Weight = {
+    value: number
+    unit: string
+}
+
+type Compliance = {
+    customs: string
+    regulatory: string
+    certificates: string[]
+}
+
+type ItemRequestBody = {
+    name: string
+    quantity: number
+    supplier: string
+    description: string
+    manufacturer: string
+    colour: string
+    category: number
+    weight: Weight
+    dimensions: Dimensions
+    handling: Handling
+    compliance: Compliance
+}
+
+const validateRequest = (request: ItemRequestBody, schema: Object): Boolean => {
+    try {
+        const ajv = new Ajv();
+        const validate = ajv.compile(schema);
+        return validate(request);
+    } catch (error) {
+        console.log('validateRequest:error', error)
+        throw new Error(error)
+    }
+};
+
+const generateTrackingId = (): String => {
+    try {
+        return `IOTA#${crypto.randomBytes(4).toString("hex")}`;
+    } catch (error) {
+        console.log("Error generating order number", error);
+    }
+}
+
+const calculateVolume = (dimensions: Dimensions): { value: string, representation: string } => {
+    const { width, height, length, unit } = dimensions;
+    const volume = parseFloat(width) * parseFloat(height) * parseFloat(length);
+
+    const value = volume.toFixed(2);
+
+    return {
+        value,
+        representation: `${value} ${unit}\u00B3`
+    }
+}
+
+export default factories.createCoreController(`${ITEM_API_PATH}`, ({ strapi }: { strapi: Strapi }) => ({
     async createNewSupplyChainItem(ctx) {
         try {
             /**
-            * (Add a Middleware) If clearance is user, block as this should come through the order route
-            * Else
-            * 1. Validate with JSON schema?
             * 2. Create item entry
             * Exit
             */
             const auth = get(ctx.state.auth, 'credentials');
             const clearance = get(auth, 'clearance');
-            const requestBody = get(ctx.request, 'body') as Object;
+            const requestBody = get(ctx.request, 'body');
+
             const isUser = clearance === 'user'
+            const isValid = validateRequest(requestBody, itemSchema)
 
             if (isUser) {
                 return ctx.forbidden('User should only use order route')
+            } else if (!isValid) {
+                return ctx.badRequest('Malformed request body')
             } else {
-                console.log(requestBody)
+                const dimensions = get(requestBody, 'dimensions')
+                let dimensionsWithVolume = {
+                    ...dimensions,
+                    volume: calculateVolume(dimensions).representation
+                };
+
+                const newItem = await strapi.entityService.create(`${ITEM_API_PATH}`, {
+                    data: {
+                        ...requestBody,
+                        ...dimensionsWithVolume,
+                        uuid: uuid(),
+                        trackingId: generateTrackingId(),
+                    },
+                    // populate: ['paper']
+                });
+                console.log(newItem);
+
                 ctx.body = {
                     success: true,
                     message: "Add item controller finished successfully"
