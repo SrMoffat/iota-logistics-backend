90aafb25523868aa765fdb75293b3df909287afe
SrMoffat <ngigemoffat@gmail.com>
2023-06-27 23:55:01 +0300

    feat(cleanup): more cleanups

--- src/api/stage/routes/index.ts
+++ src/api/stage/routes/index.ts
@@ -1,10 +1,14 @@
 export default {
-    routes: [
-      {
-        method: 'POST',
-        path: '/stages/seed',
-        handler: 'stage.seedStagesAndStatuses'
-      }
-    ]
-  };
-  
+  routes: [
+    {
+      method: 'POST',
+      path: '/stages/seed',
+      handler: 'stage.seedStagesAndStatuses'
+    },
+    {
+      method: 'GET',
+      path: '/stages/:id/events',
+      handler: 'stage.getStageEvents'
+    }
+  ]
+};
